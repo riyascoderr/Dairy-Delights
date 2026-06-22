d56cc51d38b72684b1625957e0eb51b1ab4e2d05
riyascoderr <riyas.coderr@gmail.com>
2026-06-22 18:20:31 +0530

    Update settings.py

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -21,18 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    'django-insecure-$i+0v%wgsq%q$-&qkys_f!8ex#%%v-tft)$kod42!rblg1fml-'
-)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+from dotenv import load_dotenv
+load_dotenv()
 
-ALLOWED_HOSTS =[os.environ.get(
-    'ALLOWED_HOSTS',
-    'localhost,127.0.0.1,testserver'
-).split(','),"https://dairy-delights-kvd7.onrender.com"]
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = ["dairy-delights-kvd7.onrender.com"]
 
 
 # Application definition
